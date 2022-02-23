771aa48b67246adad83cc9d41ad599a46e9bf87d
supernarubat1 <narubat1@gmail.com>
2022-02-23 17:40:46 +0700

    add get data and function clear

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.get("/api", (req, res) => {
   res.json({ message: "OK" });
 });
 
+// ADD
 app.post("/api/add", async (req, res) => {
   const text = req.body.text;
 
@@ -21,6 +22,20 @@ app.post("/api/add", async (req, res) => {
   await newTodo.save();
 
   res.json({ status: "ok", data: newTodo });
+});
+
+// GET
+app.get("/api/get", async (req, res) => {
+  const todos = await Todo.find({});
+
+  res.json({ status: "ok", data: todos });
+});
+
+// CLEAR
+app.post("/api/clear", async (req, res) => {
+  await Todo.deleteMany({});
+
+  res.json({ status: "ok" });
 });
 
 const PORT = process.env.PORT || 5000;
--- database/db.js
+++ database/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/myapp")
+    .connect(process.env.MONGO_URL)
     .then(() => console.log("Database connection..."))
     .catch((err) => console.error(err));
 };

7932574ce63aa44dcaf3273902b7ac9923ae9417
supernarubat1 <narubat1@gmail.com>
2022-02-23 23:19:12 +0700

    show env 2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/api", (req, res) => {
-  console.log(process.env.MONGO_CON_STR);
+  console.log(process.env);
   res.json({ message: "OK" });
 });
 

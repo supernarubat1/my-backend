3d51617572b6ecbfd11bb84daf04b6b136e408d8
supernarubat1 <narubat1@gmail.com>
2022-02-24 21:33:56 +0700

    fix unitest

--- database/db.js
+++ database/db.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+let MONGO_URL;
+
+if (process.env.NODE_ENV === undefined) {
+  MONGO_URL = process.env.MONGO_CON_STR_DEV;
+}
+
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test") {
+  MONGO_URL = process.env.MONGO_CON_STR_POD;
+}
+
 const connect = async () => {
   await mongoose
-    .connect(process.env.MONGO_CON_STR)
+    .connect(MONGO_URL)
     .then(() => console.log("Database connection..."))
     .catch((err) => console.error(err));
 };

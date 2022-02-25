ccac4d1098913f5663a21accb46d37606a94ee5d
supernarubat1 <narubat1@gmail.com>
2022-02-25 11:12:54 +0700

    test env

--- database/db.js
+++ database/db.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-let MONGO_URL;
+// let MONGO_URL;
 
-if (process.env.NODE_ENV === undefined) {
-  MONGO_URL = process.env.MONGO_CON_STR_DEV;
-}
+// if (process.env.NODE_ENV === undefined) {
+//   MONGO_URL = process.env.MONGO_CON_STR_DEV;
+// }
 
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test") {
-  MONGO_URL = process.env.MONGO_CON_STR_POD;
-}
+// if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test") {
+//   MONGO_URL = process.env.MONGO_CON_STR_POD;
+// }
 
 const connect = async () => {
   await mongoose
-    .connect(MONGO_URL)
+    .connect(process.env.MONGO_CON_STR_POD)
     .then(() => console.log("Database connection..."))
     .catch((err) => console.error(err));
 };
